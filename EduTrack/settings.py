a9a08603f9f885f0a93694fb80267c2c381c7774
Riz <rix0ztha@gmail.com>
2025-01-02 10:12:22 +0545

    create exception_handler and utils

--- EduTrack/settings.py
+++ EduTrack/settings.py
@@ -157,7 +157,7 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
-    "EXCEPTION_HANDLER": "config.exception_handler.new_customer_exception_handler",
+    "EXCEPTION_HANDLER": "core.exception_handler.new_customer_exception_handler",
 }
 
 SIMPLE_JWT = {
--- EduTrack/utils.py
+++ EduTrack/utils.py
@@ -1,5 +1,8 @@
+from typing import Any
+
 import jwt
 from django.core.exceptions import PermissionDenied
+from rest_framework import exceptions
 
 from EduTrack import settings
 
@@ -22,3 +25,23 @@ def get_user_id(token: str) -> str:
         return payload["user_id"]
     except Exception as e:
         raise PermissionDenied(f"An error occurred while authenticating the user. {e}")
+
+
+def get_or_not_found(qs: Any, **kwargs: Any) -> Any:
+    """
+    Retrieve a single instance from a queryset or raise a NotFound exception if not found.
+
+    Args:
+        qs: Queryset from which to retrieve the instance.
+        **kwargs: Lookup parameters to filter the queryset.
+
+    Returns:
+        An instance of the model matching the provided parameters.
+
+    Raises:
+        NotFound: If no instance matching the parameters is found.
+    """
+    try:
+        return qs.get(**kwargs)
+    except qs.model.DoesNotExist:
+        raise exceptions.NotFound(f"{qs.model.__name__} not found.")
